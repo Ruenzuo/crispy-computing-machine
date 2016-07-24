128f40d0554b4dde456482a372a110cc28dddf8c
Renzo Crisóstomo <renzo.crisostomo@xing.com>
2016-07-24 18:16:23 +0200

    Initial commit

---
+++ QuickApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickApp
+//
+//  Created by Renzo Crisóstomo on 24/07/16.
+//  Copyright © 2016 Renzo Crisóstomo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
